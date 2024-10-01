7f93623c67321cf0b9bbc3286976fe7c7f2b0d7c
ganciaux <ghislain.anciaux@live.fr>
2024-10-01 14:55:37 +0200

    Start form validation

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -1,13 +1,14 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, JsonPipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { getPokemonColor, POKEMON_RULES } from '../../pokemon.model';
 
 @Component({
   selector: 'app-pokemon-edit',
   standalone: true,
-  imports: [DatePipe, RouterLink, ReactiveFormsModule],
+  imports: [DatePipe, RouterLink, ReactiveFormsModule,JsonPipe],
   templateUrl: './pokemon-edit.component.html',
   styles: ``,
 })
@@ -20,7 +21,12 @@ export class PokemonEditComponent {
   ).asReadonly();
 
   readonly form = new FormGroup({
-    name: new FormControl(this.pokemon().name),
+    name: new FormControl(this.pokemon().name, [
+      Validators.required,
+      Validators.minLength(POKEMON_RULES.MIN_NAME),
+      Validators.maxLength(POKEMON_RULES.MAX_NAME),
+      Validators.pattern(POKEMON_RULES.NAME_PATTERN),
+    ]),
     life: new FormControl(this.pokemon().life),
     damage: new FormControl(this.pokemon().damage),
     types: new FormArray(
@@ -28,4 +34,38 @@ export class PokemonEditComponent {
     ),
   });
 
+  // Get the selected Pokemon list by user.
+get pokemonTypeList(): FormArray {
+  return this.form.get('types') as FormArray;
+}
+
+// Return if given type is already selected by user or not.
+isPokemonTypeSelected(type: string): boolean {
+  console.log("isPokemonTypeSelected:", type)
+  return !!this.pokemonTypeList.controls.find(
+    (control) => control.value === type
+  );
+}
+
+// Add or remove a given type in the selected Pokemon list.
+onPokemonTypeChange(type: string, isChecked: boolean): void {
+  if (isChecked) {
+    const control = new FormControl(type);
+    this.pokemonTypeList.push(control);
+  }
+  else {
+    const index = this.pokemonTypeList.controls
+      .map((control) => control.value)
+      .indexOf(type);
+    this.pokemonTypeList.removeAt(index);
+  }
+}
+
+getPokemonColor(type: string) {
+  return getPokemonColor(type);
+}
+
+onSubmit() {
+  console.log(this.form.value);
+}
 }
